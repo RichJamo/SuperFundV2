a59d8c10b16a5cee7afa97a11206096a242bec13
Richard Jamieson <jamieson.rich@gmail.com>
2024-09-06 10:25:04 +0200

    Rename site

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -86,7 +86,7 @@ function Header() {
       />
 
       <h1 className="text-2xl md:text-6xl font-bold tracking-tighter mb-6 text-zinc-100">
-        SuperFund
+        OmniYield
       </h1>
 
       <p className="text-zinc-300 text-base">
