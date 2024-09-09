8e9f8654701a486562539be785e2c78377c060bf
Richard Jamieson <jamieson.rich@gmail.com>
2024-09-09 14:36:25 +0200

    Prefill buy modal with USDC

--- frontend/src/components/BuyView.tsx
+++ frontend/src/components/BuyView.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { PayEmbed } from "thirdweb/react";
 import { client } from "../utils/client";
-
-
+import { arbitrum } from "thirdweb/chains";
+import { ARBITRUM_USDC_CONTRACT_ADDRESS } from "../constants";
 
 const BuyView: React.FC = ({}) => {
 
   return (
     <div className="mt-16">
 
-      <PayEmbed client={client} />
+      <PayEmbed 
+      client={client} 
+      payOptions={{
+        buyWithFiat: false,
+        prefillBuy: {
+          token: {
+            address: ARBITRUM_USDC_CONTRACT_ADDRESS,
+            name: "Arbitrum USDC",
+            symbol: "USDC",
+            // icon: "...", // optional
+          },
+          chain: arbitrum,
+          allowEdits: {
+            amount: true, // allow editing buy amount
+            token: false, // disable selecting buy token
+            chain: false, // disable selecting buy chain
+          },
+        },
+        
+      }}
+      />
 
     </div>
   );
