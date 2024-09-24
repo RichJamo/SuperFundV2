0f130d35d9ae2c6e0b7bae2315429de5337e4c90
Richard Jamieson <jamieson.rich@gmail.com>
2024-09-24 08:27:03 +0200

    Fetch pool address dynamically, rename vars

--- contracts/contracts/BaseAaveStrategy.sol
+++ contracts/contracts/BaseAaveStrategy.sol
@@ -5,31 +5,34 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IAavePool.sol";
+import "./interfaces/IAaveReceiptToken.sol";
+
 // BASE_USDC_ADDRESS = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
 // AAVE_BASE_POOL_ADDRESS = 0xA238Dd80C259a72e81d7e4664a9801593F98d1c5;
 // AAVE_BASE_USDC_CONTRACT_ADDRESS = 0x4e65fE4DbA92790696d040ac24Aa414708F5c0AB;
 
 contract BaseAaveStrategy is Ownable {
-    address public vault;
-
+    string public name;
+    address public amanaVault;
     IERC20 public inputToken;
     IAavePool public aavePool;
-    IERC20 public aaveReceiptToken;
+    IAaveReceiptToken public aaveReceiptToken;
 
     constructor(
-        address _vault,
+        string memory _name,
+        address _amanaVault,
         address _inputTokenAddress,
-        address _aavePoolAddress,
         address _aaveReceiptTokenAddress
     ) {
-        vault = _vault;
+        name = _name;
+        amanaVault = _amanaVault;
         inputToken = IERC20(_inputTokenAddress);
-        aavePool = IAavePool(_aavePoolAddress);
-        aaveReceiptToken = IERC20(_aaveReceiptTokenAddress);
+        aaveReceiptToken = IAaveReceiptToken(_aaveReceiptTokenAddress);
+        aavePool = IAavePool(aaveReceiptToken.POOL());
     }
 
     modifier onlyVault() {
-        require(msg.sender == vault, "Only vault can call");
+        require(msg.sender == amanaVault, "Only amanaVault can call");
         _;
     }
 
