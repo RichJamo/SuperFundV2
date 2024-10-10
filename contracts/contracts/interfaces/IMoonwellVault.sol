0fdc0c2a36a19992d7a2c0508e61a978ab3c2c27
Richard Jamieson <jamieson.rich@gmail.com>
2024-10-10 09:22:53 +0200

    Add return value to withdraw function in moonwell interface

--- contracts/contracts/interfaces/IMoonwellVault.sol
+++ contracts/contracts/interfaces/IMoonwellVault.sol
@@ -3,7 +3,14 @@ pragma solidity 0.8.26;
 
 interface IMoonwellVault {
     function deposit(uint256 assets, address receiver) external;
-    function withdraw(uint256 assets, address receiver, address owner) external;
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) external returns (uint256);
+
     function balanceOf(address account) external view returns (uint256);
+
     function convertToAssets(uint256 shares) external view returns (uint256);
 }
